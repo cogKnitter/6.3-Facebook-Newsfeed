bb767f9469f80bb7ed11528eb9639728d03cd72c
Mariah Tree <mariah.tree@gmail.com>
2016-05-18 18:38:13 -0500

    added initial styling

--- modules/Article.js
+++ modules/Article.js
@@ -5,7 +5,7 @@ export default React.createClass({
     return{
       author_name: "Sasquatch",
       author_img: "../images/sassy.jpg",
-      text: "Sasquatches are the best",
+      text: "Sasquatches are the best! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       date: "May 18th"
     }
   },
@@ -13,13 +13,13 @@ export default React.createClass({
   render() {
     return (
         <article class="main__article">
-          <div>
-           <img src="../images/sassy.jpg"/>
-           <h2>{this.props.author_name}</h2>
-           <h3>{this.props.date}</h3>
+          <div className="op__detail--wrapper">
+           <img className="op__avatar" src="../images/sassy.jpg"/>
+             <h2 className="op__name">{this.props.author_name}</h2>
+             <h3 className="op__date">{this.props.date}</h3>
           </div>
-          <div>
-            <p>
+          <div className="op__text--wrapper">
+            <p className="op__text">
               {this.props.text}
             </p>
           </div>
